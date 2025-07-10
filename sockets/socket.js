ffb6c03f75c8c46c1ed597f435209efbdd01f151
Francisco Espinoza <fdj.espinoza@gmail.com>
2025-07-09 22:36:19 -0700

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,13 @@
+const { io } = require('../index');
+
+// Sockets
+io.on('connection', (client) => {
+    console.log('Client connected');
+    client.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+    client.on('message', (payload) => {
+        console.log('Message received:', payload);
+        io.emit('message', { admin: 'New message received' });
+    });
+});
